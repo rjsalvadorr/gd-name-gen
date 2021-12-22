1744cbe407d4d8306f3101eef6ba2f2ce6a61560
RJ Salvador <rjsalvadorr@gmail.com>
2021-12-22 15:23:20 -0500

    completed business prompt gen

--- business-graphic-gen.py
+++ business-graphic-gen.py
@@ -5,11 +5,15 @@ from datetime import datetime, timedelta
 #/////////#/////////#/////////#/////////#/////////#/////////
 #///////////   LOADING FILES
 
-bandNames = open("band-names.txt", "r", encoding='utf-8')
-albumNames = open("album-names.txt", "r", encoding='utf-8')
-cityNames = open("city-names.txt", "r", encoding='utf-8')
+businessNames = open("business-names.txt", "r", encoding='utf-8')
+businessIndustryList = open("business-industry-list.txt", "r", encoding='utf-8')
+
 countryNames = open("country-names.txt", "r", encoding='utf-8')
-genreNames = open("genre-list.txt", "r", encoding='utf-8')
+countryCodes = open("country-codes.txt", "r", encoding='utf-8')
+cityNames = open("city-names.txt", "r", encoding='utf-8')
+streetNames = open("street-names.txt", "r", encoding='utf-8')
+
+personNames = open("person-names.txt", "r", encoding='utf-8')
 
 
 #/////////#/////////#/////////#/////////#/////////#/////////
@@ -31,7 +35,10 @@ def random_date(start, end):
     int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
     random_second = randrange(int_delta)
     return start + timedelta(seconds=random_second)
-	
+
+def random_num_string():
+    rNum = random.randint(1, 9999)
+    return f'{rNum:04}'
 	
 #/////////#/////////#/////////#/////////#/////////#/////////
 #///////////    TEXT GENERATION
@@ -39,16 +46,14 @@ def random_date(start, end):
 outStr = '\n////////////////////////////////////////////////////////////\n'
 outStr += '//////   GENERATOR -- BUSINESS GRAPHIC BRIEF\n\n'
 
-# outStr += f'BAND NAME: {random_line(bandNames).rstrip()}\n'
-# bandNames.seek(0)
-# outStr += f'ALBUM NAME: {random_line(bandNames).rstrip()}\n'
-# bandGenres = 'GENRE(S): '
-# for x in range(4):
-#     genreNames.seek(0)
-#     delim = (",", "")[x == 0]
-#     bandGenres += f'{delim} {random_line(genreNames).rstrip()}'
-# outStr += f'{bandGenres}\n'
-# 
+outStr += f'BUSINESS NAME: {random_line(businessNames).rstrip()}\n'
+
+busIndustries = '\nINDUSTRIES/BUSINESS SECTORS: \n\n'
+for x in range(3):
+    businessIndustryList.seek(0)
+    busIndustries += f'- {random_line(businessIndustryList).rstrip()}\n'
+outStr += f'{busIndustries}\n'
+
 # outStr += '\nTRACKLIST: \n'
 # numTracks = random.randint(6,13)
 # 
@@ -67,15 +72,44 @@ outStr += '//////   GENERATOR -- BUSINESS GRAPHIC BRIEF\n\n'
 # tNames += f' - OR - {random_line(albumNames).rstrip()}'
 # outStr += f'\nTOUR NAME:\n{tNames}\n'
 # 
-# outStr += '\nTOUR STOPS: \n'
-# numCities = random.randint(6,20)
-# tourStart = datetime.strptime('1/1/2032 1:30 PM', '%m/%d/%Y %I:%M %p')
-# tourEnd = datetime.strptime('12/30/2099 4:50 AM', '%m/%d/%Y %I:%M %p')
-# 
-# for x in range(numCities):
-#     cityNames.seek(0)
-#     countryNames.seek(0)
-#     eventDay = random_date(tourStart, tourEnd).strftime("%b %d, %Y")
-#     outStr += f'{eventDay} - {random_line(cityNames).rstrip()}, {random_line(countryNames).rstrip()}\n'
+
+outStr += 'HEAD OFFICE: \n\n'
+streetNames.seek(0)
+cityNames.seek(0)
+countryNames.seek(0)
+
+addrNum = random.randint(1, 3999)
+addrStreet = random_line(streetNames).rstrip()
+addrCity = random_line(cityNames).rstrip()
+addrCountry = random_line(countryNames).rstrip()
+addrPhone = f'{random_num_string()}-{random_num_string()}{random_num_string()}'
+
+headOfficeLocation = f'{addrNum} {addrStreet}\n{addrCity}, {addrCountry}\n{addrPhone}'
+outStr += f'{headOfficeLocation}\n\n'
+
+numLocations = random.randint(0,4)
+outStr += ('', 'OTHER LOCATIONS: \n\n')[numLocations > 0]
+for x in range(numLocations):
+    streetNames.seek(0)
+    cityNames.seek(0)
+    countryNames.seek(0)
+
+    sattNum = random.randint(1, 3999)
+    sattStreet = random_line(streetNames).rstrip()
+    sattCity = random_line(cityNames).rstrip()
+    sattCountry = random_line(countryNames).rstrip()
+    sattPhone = f'{random_num_string()}-{random_num_string()}{random_num_string()}'
+
+    satelliteLocation = f'{sattNum} {sattStreet}\n{sattCity}, {sattCountry}\n{sattPhone}'
+    outStr += f'{satelliteLocation}\n\n'
+
+
+numPersonnel = random.randint(2, 8)
+outStr += 'PERSONNEL: \n\n'
+for x in range(numPersonnel):
+    personNames.seek(0)
+    pName = random_line(personNames).rstrip()
+    pPhone = f'{random_num_string()}-{random_num_string()}{random_num_string()}'
+    outStr += f'{pName}\n{pPhone}\n\n'
 
 print(outStr)
